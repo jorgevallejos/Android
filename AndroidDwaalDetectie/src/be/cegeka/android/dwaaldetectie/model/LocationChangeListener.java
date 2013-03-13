24021b3bc6593b1adef98f64f7edc491b159c1ab
Hannes Crombez <hannes.crombez@cegeka.be>
2013-03-13 11:49:09 +0100

    ...

---
+++ AndroidDwaalDetectie/src/be/cegeka/android/dwaaldetectie/model/LocationChangeListener.java
@@ -0,0 +1,65 @@
+package be.cegeka.android.dwaaldetectie.model;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.widget.Toast;
+
+
+public class LocationChangeListener implements LocationListener
+{
+	private Context context;
+	private Location baseLocation;
+	
+	
+	public LocationChangeListener(Context context, Location baseLocation)
+	{
+		this.context = context;
+		this.baseLocation = baseLocation;
+	}
+
+
+	@Override
+	public void onLocationChanged(Location location)
+	{
+		// Get Longitude and latitude from current location.
+		double longitude = location.getLongitude();
+		double latitude = location.getLatitude();
+
+		// Print to console
+		System.out.println("Longitude Current Address: " + longitude);
+		System.out.println("Latitude Current Address: " + latitude);
+
+		System.out.println(baseLocation.distanceTo(location));
+
+		if (baseLocation != null && baseLocation.distanceTo(location) > 2000)
+		{
+			Toast.makeText(context, "Too far", Toast.LENGTH_LONG).show();
+		}
+	}
+
+
+	@Override
+	public void onProviderDisabled(String provider)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+
+	@Override
+	public void onProviderEnabled(String provider)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras)
+	{
+		// TODO Auto-generated method stub
+
+	}
+}
